97b4ff1abc495fcd2ae69851db14b23c45e0b254
akash <akashkamble6226@gmail.com>
2022-01-06 23:39:35 +0530

    added blurred tabbar

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:tipsandtricks/widgets/adding_animated_icon.dart';
 import 'package:tipsandtricks/widgets/adding_animated_list.dart';
 import 'package:tipsandtricks/widgets/adding_animated_widget.dart';
 import 'package:tipsandtricks/widgets/adding_aspect_ratio.dart';
+import 'package:tipsandtricks/widgets/adding_blurred_tabbar.dart';
 import 'package:tipsandtricks/widgets/adding_floating_action_button.dart';
 import 'package:tipsandtricks/widgets/adding_jitter_free_screens.dart';
 import 'package:tipsandtricks/widgets/adding_position_change.dart';
@@ -26,10 +27,9 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const BlurredImage(),
+      home:const BlurredTabBar(
+        
+      ),
     );
   }
 }
-
-
-
